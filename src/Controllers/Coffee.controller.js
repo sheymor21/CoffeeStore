c88f37bfb70dc2104cf4c8d6a813f5f1b29df5b3
Sheymor <joseacvz81@gmail.com>
2024-09-14 18:24:58 -0400

    Add full CRUD operations for Coffee resource
    Implemented `getCoffee`, `updateCoffee`, and `deleteCoffee` methods in the Coffee controller. Updated Coffee route to handle GET, PUT, and DELETE requests. This enhances the Coffee API to support complete CRUD functionality.

--- src/Controllers/Coffee.controller.js
+++ src/Controllers/Coffee.controller.js
@@ -7,13 +7,53 @@ const addCoffee = async (req, res) => {
         if (error) {
             return res.status(400).send({
                 error: error.details[0].message
-           })
+            })
         }
         const test = await Coffee.create(req.body, null)
-        res.status(201).send(test)
+        return res.status(201).send(test)
     } catch (err) {
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
 }
 
-module.exports = {addCoffee}
+const getCoffee = async (req, res) => {
+    try {
+        const projection = {_id: 1, name: 1, type: 1, size: 1, ingredients: 1, price: 1}
+        const coffeeList = await Coffee.find({}, projection, null);
+        return res.status(200).send(coffeeList);
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+
+const updateCoffee = async (req, res) => {
+    const {error} = CoffeeValidation.validate(req.body);
+    if (error) {
+        return res.status(400).send({
+            error: error.details[0].message
+        })
+    }
+    const {id} = req.params;
+    const updatedCoffee = await Coffee.findByIdAndUpdate(id, req.body, null);
+    if (!updatedCoffee) {
+        return res.status(404).send({
+            error: "Coffee not found"
+        });
+    }
+    return res.status(200).send('update coffee');
+
+}
+
+const deleteCoffee = async (req, res) => {
+    const {id} = req.params
+    const deleteConfirmation = await Coffee.findByIdAndDelete(id, null);
+    if (!deleteConfirmation) {
+        return res.status(404).send({
+            error: "Coffee not found"
+        })
+    }
+    return res.status(200).send('deleted coffee');
+
+}
+
+module.exports = {addCoffee, getCoffee, updateCoffee, deleteCoffee}
--- src/Routes/Coffee.route.js
+++ src/Routes/Coffee.route.js
@@ -1,8 +1,11 @@
 ﻿const express = require('express');
 const router = express.Router();
-const {addCoffee} = require('../Controllers/Coffee.controller');
+const {addCoffee, getCoffee, updateCoffee, deleteCoffee} = require('../Controllers/Coffee.controller');
 
 
 router.post('/', addCoffee)
+router.get('/', getCoffee);
+router.put('/:id', updateCoffee);
+router.delete('/:id', deleteCoffee);
 
 module.exports = router;
